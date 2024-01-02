6964d5d1d1c05dc8a302d98ce0965c3449934161
Joan Filipe <joanfilipe0@gmail.com>
2024-01-02 16:22:00 -0300

    Configurando o Cypress Aula 01

---
+++ cypress/e2e/CAT-CAT.cy.js
@@ -0,0 +1,10 @@
+/// <reference types="Cypress" />
+
+describe('Central de Atendimento ao Cliente TAT', () => {
+
+  it('verifica o título da aplicação', () => {
+    cy.visit('./src/index.html');
+    cy.title().should('eq', 'Central de Atendimento ao Cliente TAT');
+  })
+  
+});

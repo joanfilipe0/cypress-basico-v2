5158f8fb291971d5cffe3bf670135e74a7183e8d
Joan Filipe <joanfilipe0@gmail.com>
2024-01-03 10:21:52 -0300

    Exercício

--- cypress/e2e/CAT-CAT.cy.js
+++ cypress/e2e/CAT-CAT.cy.js
@@ -2,9 +2,34 @@
 
 describe('Central de Atendimento ao Cliente TAT', () => {
 
-  it('verifica o título da aplicação', () => {
+  // Código a ser executado antes de cada teste
+  beforeEach(() => {
     cy.visit('./src/index.html');
+  })
+
+  it('verifica o título da aplicação', () => {
     cy.title().should('eq', 'Central de Atendimento ao Cliente TAT');
   })
-  
+
+  // Exercicio
+  it('preenche os campos obrigatórios e envia o formulário', () => {
+    
+    // Preenche os campos obrigatórios
+    cy.get('#firstName').type('SeuNome');
+    cy.get('#lastName').type('SeuSobrenome');
+    cy.get('#email').type('seuemail@example.com');
+    cy.get('#open-text-area').type('Mensagem de teste para ajuda.');
+
+    // Marca a opção de contato preferencial
+    cy.get('#email-checkbox').check();
+
+    // Clica no botão Enviar
+    cy.get('.button[type="submit"]').click();
+
+    // Verifica se a mensagem de sucesso está visível
+    cy.get('.success').should('be.visible');
+  });
+
+
+
 });
